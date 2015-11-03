bdfe7f3cf2b955c5f6b3732df7dd055a4aa77c62
Bernardo Rittmeyer <bernardo@rittme.com>
2015-11-03 20:10:05 +0100

    removed unwanted files

---
+++ Exercices/Exercice1/exercice1.hs
@@ -0,0 +1,8 @@
+{- The sell rate for the English pound was
+   12.7775 SEK on the 20th of October 2008.
+   How many pounds did one get for 1000 SEK that day?
+-}
+
+exchangeRate = 12.7775
+
+toLBS p = s / exchangeRate
